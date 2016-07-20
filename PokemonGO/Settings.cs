6706c10160cf0950f802f382494127c1dab27fd6
Charles Jones <Charles Jones>
2016-07-20 14:36:50 -0400

    no message

--- PokemonGO/Settings.cs
+++ PokemonGO/Settings.cs
@@ -8,14 +8,14 @@ namespace PokemonGO
 {
     internal static class Settings
     {
-        internal static readonly string PTC_USERNAME = "neznamsikoi";
-        internal static readonly string PTC_PASSWORD = "neznam";
+        internal static readonly string PTC_USERNAME = "Your PTC Username";
+        internal static readonly string PTC_PASSWORD = "Your PTC Password";
 
         internal static readonly int EXPLORATION_STEPS = 30; // Number of steps (increase this to explore a wider area)
         internal static readonly int STEP_DELAY = 1; // Seconds between steps
         internal static readonly int CLEAR_DELAY = 30; // Seconds between clearing map
 
-        internal static readonly string STARTING_LOCATION = "530 Means St Atlanta, GA 30318"; // Example: Time Square, New York City
+        internal static readonly string STARTING_LOCATION = "Starting Location"; // Example: Time Square, New York City
 
         internal static readonly int POKEMON_IMAGE_SIZE = 40;
     }
